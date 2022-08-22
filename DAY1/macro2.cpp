281fb1adc1c31fa63bee49437efef17c8989824e
ecourse <smkang@codenuri.co.kr>
2022-08-22 15:30:34 +0900

    Add files via upload

---
+++ DAY1/macro2.cpp
@@ -0,0 +1,21 @@
+// macro2.cpp
+#include <cstdio>
+
+#define SQUARE(x)   (x) * (x)
+
+int main()
+{
+	int a = 3;
+	
+//	int ret = SQUARE(a+1); // (a + 1) * (a + 1)
+
+	int ret = SQUARE(++a); // (++a) * (++a) => 25
+
+	printf("%d\n", ret); // 결과 예측해 보세요
+}
+// 결론 : 매크로는 "문자 치환" 방식입니다.
+//       버그가 너무 많습니다.
+//		 위 SQUARE 는 인자로 "++a"를 사용하면 버그 입니다.
+//		 SQUARE 매크로를 아무리 잘만들어도 이 문제를 해결할수 없습니다.
+
+// cl macro2.cpp /EP
