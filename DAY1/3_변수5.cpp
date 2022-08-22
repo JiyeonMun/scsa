ff2a9260665f6f3b5fca7866a5c9a28959234183
ecourse <smkang@codenuri.co.kr>
2022-08-22 12:26:34 +0900

    Add files via upload

---
+++ DAY1/3_변수5.cpp
@@ -0,0 +1,16 @@
+﻿// 3_변수5
+// 23 page ~
+
+// typedef : 기존 타입의 별칭을 만드는 문법
+// using   : 기존 타입의 별칭 + "템플릿" 이라는 것의 별칭
+
+//typedef int DWORD;
+
+// C++11 부터, typedef 대신 using 사용하세요
+using DWORD = int; // 위와 동일합니다.
+
+int main()
+{
+	DWORD n; // int n
+}
+
