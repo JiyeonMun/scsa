9ba842161b2f8b3d78b46570de5283becae1c530
ecourse <smkang@codenuri.co.kr>
2022-08-23 14:38:46 +0900

    Add files via upload

---
+++ DAY2/5_생성자2-2.cpp
@@ -0,0 +1,49 @@
+// 5_생성자1.cpp
+#include <iostream>
+#include <string>
+
+class Person
+{
+private:
+	std::string name;
+	int  age;
+public:
+	Person(const std::string& n, int a)
+	{
+		std::cout << "생성자 호출됨" << std::endl;
+		name = n;
+		age = a;
+	}
+	Person()
+	{
+		name = "unknown";
+		age = 0;
+	}
+	// 소멸자 : "~클래스이름" 모양의 함수
+	//			반환값과 인자가 없다. 오직 한개만 만들수 있다.
+	//		    객체가 파괴 될때 호출.
+	~Person() { std::cout << "소멸자" << std::endl; }
+};
+int main()
+{
+	// 모든 지역 변수(객체)는 자신을 선언한 블럭을 벗어 날때 파괴 됩니다.
+	{
+		Person p2;
+		
+		{
+
+			Person p1("kim", 28);
+			std::cout << "1st block end" << std::endl;
+
+		}	// <= p1 파괴, 소멸자 호출
+
+		
+		std::cout << "2st block end" << std::endl;
+
+	}	// <= p2 파괴 소멸자 호출
+
+	std::cout << "main 종료" << std::endl;
+} 
+
+
+
