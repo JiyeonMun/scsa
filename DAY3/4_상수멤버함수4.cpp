aac4814cd836e6f80e6ed7941fbfb659b894fad0
ecourse <smkang@codenuri.co.kr>
2022-08-24 14:31:47 +0900

    Add files via upload

--- DAY3/4_상수멤버함수4.cpp
+++ DAY3/4_상수멤버함수4.cpp
@@ -6,12 +6,15 @@ public:
 	int x, y;
 
 	Point(int a = 0, int b = 0);
+
 	void set(int a, int b);
 	void print() const;
 };
 // 핵심 1. 상수 멤버 함수는 "선언과 구현에 모두 표기해야 합니다."
+// 2. 디폴트 파라미터는 선언에만 표기하세요
+// 3. 생성자의 멤버 초기화 리스트는 구현쪽에 표기해야 합니다.
 
-Point::Point(int a = 0, int b = 0) : x(a), y(b) 
+Point::Point(int a /* = 0*/, int b /*= 0*/) : x(a), y(b) 
 {
 }
 void Point::set(int a, int b) 
