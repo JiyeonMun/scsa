edd0f16680ef933e01c4a67c0ad0e7b64fcaacda
ecourse <smkang@codenuri.co.kr>
2022-08-24 15:11:49 +0900

    Add files via upload

--- DAY3/배열과포인터2.cpp
+++ DAY3/배열과포인터2.cpp
@@ -2,13 +2,14 @@
 
 int main()
 {
-	std::cout << sizeof(int) << std::endl;
-	std::cout << sizeof(char) << std::endl;
-	std::cout << sizeof(double) << std::endl;
+	std::cout << sizeof(int) << std::endl;		// 4 byte
+	std::cout << sizeof(char) << std::endl;		// 1 byte
+	std::cout << sizeof(double) << std::endl;	// 8 byte
 
-
-	std::cout << sizeof(int*) << std::endl;
-	std::cout << sizeof(char*) << std::endl;
-	std::cout << sizeof(double*) << std::endl;
+	// 모든 종류의 포인터는 크기가 동일 합니다.
+	// 32bit : 4byte,   64bit : 8 byte
+	std::cout << sizeof(int*) << std::endl;		// 8
+	std::cout << sizeof(char*) << std::endl;	// 8
+	std::cout << sizeof(double*) << std::endl;	// 8
 
 }
