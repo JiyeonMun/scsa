6e742132df3852250298ef1033116a7c2c7f441e
ecourse <smkang@codenuri.co.kr>
2022-08-25 17:49:27 +0900

    Add files via upload

--- DAY4/temporary3.cpp
+++ DAY4/temporary3.cpp
@@ -4,11 +4,14 @@ class Counter
 {
 	int cnt = 0;
 public:
-	Counter increment()
+	// 결론, *this 반환 하는 함수는 반드시 참조 반환해야 합니다.
+//	Counter increment()
+	Counter& increment()
 	{
 		++cnt;
 		return *this;
 	}
+	~Counter() { std::cout << "~Counter" << std::endl; }
 	int get() const { return cnt; }
 };
 int main()
@@ -20,4 +23,13 @@ int main()
 	c.increment().increment().increment(); 
 
 	std::cout << c.get() << std::endl; // ?
+}
+
+// 주의 사항!!
+// 지역변수는 절대 참조 반환하면 안됩니다.
+Counter& fn()
+{
+	Counter c;
+	return c; // 버그.. 절대 참조 반환하면 안됩니다.
+				// 반드시, 값반환만 사용해야 합니다.
 }
