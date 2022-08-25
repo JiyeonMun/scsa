ffb731bad8f115a306abb604f634f5a4f92349b1
ecourse <smkang@codenuri.co.kr>
2022-08-25 11:13:36 +0900

    Add files via upload

---
+++ DAY4/2_상속5.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+
+class Person
+{
+	std::string name;
+	int age;
+public:
+	Person(const std::string& s, int a) : name(s), age(a)
+	{
+	}
+};
+// Person 에서 상속받는 Student 만드세요
+// => int id 추가하세요
+// => 생성자에서 초기화하는 코드 만드세요..
+
+int main()
+{
+//	Person p;	// 이렇게 만들수 있게 하면
+				// => 초기화되지 않은 객체의 생성입니다. 좋지 않습니다.
+
+	Person p("kim", 20); // ok
+}
+
+
+
