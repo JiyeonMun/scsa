c2852d74725c38da0c499c883f99077ba27a187d
ecourse <smkang@codenuri.co.kr>
2022-08-25 17:39:44 +0900

    Add files via upload

--- DAY4/temporary2.cpp
+++ DAY4/temporary2.cpp
@@ -11,11 +11,15 @@ public:
 };
 //-----------------------
 int x = 10;
+int  f1() { return x; } // x 자체의 반환이 아닌 "10" 반환 입니다.
+int& f2() { return x; } // x 의 별명 반환
+
+
 Point pt(1, 2);
-
-int f1() { return x; }
-
-Point f3() { return pt; }
+//Point f3() { return pt; } // pt 자체의 반환이 아닌 
+							// pt 를 복사한 "리턴용도의 임시객체"가 반환됨
+							// 
+Point& f3() { return pt; } // 임시객체가 아닌 진짜 pt 반환(별명)
 
 int main()
 {
