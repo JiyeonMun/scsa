6f38d4bb1a3e2599a2f297bb2230215e8b0a31a1
ecourse <smkang@codenuri.co.kr>
2022-08-25 17:32:59 +0900

    Add files via upload

--- DAY4/temporary1.cpp
+++ DAY4/temporary1.cpp
@@ -11,7 +11,16 @@ public:
 };
 int main()
 {
-	Point pt(1, 2);
+//	Point pt(1, 2); // named object, 수명 : 자신을 선언한 블럭을 벗어날때 파괴
+
+//	Point (1, 2);   // unnamed object, 
+					// 임시객체(temporary) 라고 합니다.
+					// 수명 : 자신을 선언한 문장의 끝에서 파괴.
+
+
+	Point(1, 2), std::cout << "X" << std::endl;
 
 	std::cout << "---------------" << std::endl;
 }
+
+// 화면 출력결과 예측해 보세요.
