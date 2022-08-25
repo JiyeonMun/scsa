8d03f065ff08f82dd23898ad5765669d6f5ea72f
ecourse <smkang@codenuri.co.kr>
2022-08-25 11:14:54 +0900

    Add files via upload

---
+++ DAY4/2_상속5-1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+class Person
+{
+	std::string name;
+	int age;
+public:
+	Person(const std::string& s, int a) : name(s), age(a)
+	{
+	}
+};
+// Student 만드세요.
+
+int main()
+{
+	// Student 객체 생성 하는 코드도 만드세요
+}
+
+
+
