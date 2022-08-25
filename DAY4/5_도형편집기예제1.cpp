1e9ea09d8cf9f69dcbed33fb1e65711573431f0a
ecourse <smkang@codenuri.co.kr>
2022-08-25 14:05:07 +0900

    Add files via upload

---
+++ DAY4/5_도형편집기예제1.cpp
@@ -0,0 +1,42 @@
+﻿// 예제1     152 page
+#include <iostream>
+#include <vector>
+
+// 핵심 1. 각 도형을 타입으로 설계하면 편리합니다.
+
+class Rect
+{
+public:
+	void draw() { std::cout << "draw Rect" << std::endl; }
+};
+
+class Circle
+{
+public:
+	void draw() { std::cout << "draw Circle" << std::endl; }
+};
+
+int main()
+{
+	int cmd;
+
+	while (1)
+	{
+		std::cin >> cmd;
+		if (cmd == 1)
+		{
+			// 사각형 생성
+		}
+		else if (cmd == 2)
+		{
+			// 원 생성
+		}
+		else if (cmd == 9)
+		{
+			// 모든 도형 그리기. 
+		}
+	}
+}
+
+
+
